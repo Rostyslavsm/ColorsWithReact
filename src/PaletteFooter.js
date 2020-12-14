7f985b16e63e280b5a6a4f4639c97f41f6cae242
Rostyslav Muretov <rostyslav@Rostyslavs-MacBook-Pro.local>
2020-12-14 01:34:14 -0400

    add go back link

--- src/PaletteFooter.js
+++ src/PaletteFooter.js
@@ -1,5 +1,3 @@
-import react from "react";
-
 function PaletteFooter (props) {
     
     const {paletteName , emoji} = props;
--- src/SingleColorPalette.js
+++ src/SingleColorPalette.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import ColorBox from "./colorBox";
 import Navbar from "./Navbar";
 import PaletteFooter from "./PaletteFooter";
+import { Link } from "react-router-dom";
 
 export default class SingleColorPalette extends Component {
      constructor(props){
@@ -29,17 +30,22 @@ export default class SingleColorPalette extends Component {
 
     render() {
         const { format }=this.state;
-        const { paletteName , emoji }=this.props.palette;
+        const { paletteName , emoji , id }=this.props.palette;
         const colorBoxes = this._shades.map(color=>
             <ColorBox color={color.hex} 
-                        key={color.id} 
+                        key={color.name} 
                         background={color[format]} 
                         showLink={false}/>
         )
         return (
-            <div className="Palette">
+            <div className="SingleColorPalette Palette">
                 <Navbar handleChange={this.changeFormat} showingAllColors={false}/>
-                <div className="Palette-colors">{colorBoxes}</div>
+                <div className="Palette-colors">
+                    {colorBoxes}
+                    <div className="go-back ColorBox">
+                        <Link className="back-button" to={`/palette/${id}`}>GO BACK</Link>
+                    </div>
+                </div>
                 <PaletteFooter paletteName={paletteName} emoji={emoji}/>
             </div>
         )
